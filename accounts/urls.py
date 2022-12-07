4a01faed83ea70b84e7314e69e68d3fbe7c11df2
Miguel <miguel.almeida1009@gmail.com>
2022-12-07 08:18:35 -0300

    Accounts app created.

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('register/', views.register_view, name='register'),
+    path('register/create/', views.register_create, name='register_create'),
+    path('login/', views.login_view, name='login'),
+    path('login/create/', views.login_create, name='login_create'),
+]
---
+++ accounts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def register_view(request):
+    ...
+
+
+def register_create(request):
+    ...
+
+
+def login_view(request):
+    ...
+
+
+def login_create(request):
+    ...
