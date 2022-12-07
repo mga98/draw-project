06c7efa64b4bb4463e34953cf3f78412172bfdd3
Miguel <miguel.almeida1009@gmail.com>
2022-12-07 08:28:18 -0300

    Accounts app configuration.

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def register_view(request):
-    ...
+    return render(request, 'accounts/pages/register_view.html')
 
 
 def register_create(request):
@@ -10,7 +10,7 @@ def register_create(request):
 
 
 def login_view(request):
-    ...
+    return render(request, 'accounts/pages/login_view.html')
 
 
 def login_create(request):
