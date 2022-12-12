0b4ff771eae3fe6f3151dcf6cb4b4d5bfedbacc5
Miguel Almeida <miguel.almeida1009@gmail.com>
2022-12-12 10:46:26 -0300

    Register view tests

--- accounts/tests/test_register_view.py
+++ accounts/tests/test_register_view.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-# from django.test import TestCase as DjangoTestCase
+from django.test import TestCase as DjangoTestCase
 from django.urls import resolve, reverse
 from parameterized import parameterized
 
@@ -56,3 +56,101 @@ class RegisterViewUnitTest(TestCase):
         current_label = form[field].field.label
 
         self.assertEqual(label, current_label)
+
+
+class AccountRegisterIntegrationTest(DjangoTestCase):
+    def setUp(self, *args, **kwargs):
+        self.form_data = {
+            'username': 'user',
+            'first_name': 'first',
+            'last_name': 'last',
+            'email': 'email@email.com',
+            'password': 'Password12.',
+            'password2': 'Password12.'
+        }
+
+        return super().setUp(*args, **kwargs)
+
+    @parameterized.expand([
+        ('username', 'Você precisa preencher o campo de usuário!'),
+        ('first_name', 'Você precisa preencher o campo de primeiro nome!'),
+        ('last_name', 'Você precisa preencher o campo de último nome!'),
+        ('email', 'Você precisa preencher o campo de E-mail!'),
+        ('password', 'Você precisa preencher o campo da senha'),
+        ('password2', 'Você precisa repetir sua senha!'),
+    ])
+    def test_register_fields_cannot_be_empty(self, field, msg):
+        url = reverse('accounts:register_create')
+        self.form_data[field] = ''
+
+        response = self.client.post(url, data=self.form_data, follow=True)
+        self.assertIn(msg, response.content.decode('utf-8'))
+
+    def test_username_min_length(self):
+        url = reverse('accounts:register_create')
+        self.form_data['username'] = '123'
+        msg = 'O nome de usuário precisa ter pelo menos 4 caracteres!'
+
+        response = self.client.post(url, data=self.form_data, follow=True)
+        self.assertIn(msg, response.content.decode('utf-8'))
+
+    def test_username_max_length(self):
+        url = reverse('accounts:register_create')
+        self.form_data['username'] = 'A' * 16
+        msg = 'O nome de usuário pode ter no máximo 15 caracteres!'
+
+        response = self.client.post(url, data=self.form_data, follow=True)
+        self.assertIn(msg, response.content.decode('utf-8'))
+
+    def test_password_strength(self):
+        url = reverse('accounts:register_create')
+        self.form_data['password'] = 'password'
+        msg = 'Sua senha deve ter no mínimo uma letra maiúscula, '
+        'uma letra minúsucla e um número. '
+        'E deve ter no mínimo 8 carateres.'
+
+        response = self.client.post(url, data=self.form_data, follow=True)
+        self.assertIn(msg, response.content.decode('utf-8'))
+
+    def test_passwords_matchs(self):
+        url = reverse('accounts:register_create')
+        self.form_data['password'] = 'Nomatch12.'
+        msg = 'Verifique se as senhas são iguais'
+
+        response = self.client.post(url, data=self.form_data, follow=True)
+        self.assertIn(msg, response.content.decode('utf-8'))
+
+    def test_send_request_to_register_create_returns_404(self):
+        url = reverse('accounts:register_create')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_email_must_be_unique(self):
+        url = reverse('accounts:register_create')
+
+        self.client.post(url, data=self.form_data, follow=True)
+        response = self.client.post(url, data=self.form_data, follow=True)
+        msg = 'Este E-mail já está sendo utilizado!'
+
+        self.assertIn(msg, response.context['form'].errors.get('email'))
+        self.assertIn(msg, response.content.decode('utf-8'))
+
+    def test_form_is_valid(self):
+        url = reverse('accounts:register_create')
+
+        response = self.client.post(url, data=self.form_data, follow=True)
+        msg = 'Você foi registrado e logado com sucesso!'
+
+        self.assertIn(msg, response.content.decode('utf-8'))
+
+    def test_account_created_can_login(self):
+        url = reverse('accounts:register_create')
+        self.client.post(url, data=self.form_data, follow=True)
+
+        is_authenticated = self.client.login(
+            username='user',
+            password='Password12.'
+        )
+
+        self.assertTrue(is_authenticated)
