746747daee277104d98fd04f9c49edadce4e1710
Miguel Almeida <miguel.almeida1009@gmail.com>
2022-12-27 09:56:00 -0300

    Dotenv added for security

--- drawproject/settings.py
+++ drawproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 # flake8: noqa
+import os
 
 from pathlib import Path
 from django.contrib.messages import constants
@@ -22,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1or%iu-&*o12=#zxs8^&mihdo-fp=^x(ina#*-bma2v97@z#q9'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS = ['*']
 
