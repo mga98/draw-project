39e820c31f77ab0803bc4d2eede3d014e8f2b8b8
Miguel Almeida <miguel.almeida1009@gmail.com>
2023-02-22 17:41:17 -0300

    draws model serializer

--- draws/serializers.py
+++ draws/serializers.py
@@ -1,21 +1,20 @@
 from rest_framework import serializers
 
 from accounts.models import Profile
+from .models import Draw
 
 
-class DrawSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=35)
-    description = serializers.CharField(max_length=40)
-    is_published = serializers.BooleanField()
+class DrawSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Draw
+        fields = [
+            'id', 'title', 'description', 'is_published',
+            'author', 'liked_ids', 'like_count'
+        ]
+
     author = serializers.StringRelatedField()
     liked_ids = serializers.PrimaryKeyRelatedField(
         source='like',
         queryset=Profile.objects.all(),
         many=True,
     )
-    like_count = serializers.IntegerField()
-
-    def profile_method(self, profile):
-        user = profile.objects.all()
-        return user.likes()
