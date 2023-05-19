ae16e45e6f077c59834d7530f31f35c401531d0a
Miguel Almeida <miguel.almeida1009@gmail.com>
2023-05-19 10:38:16 -0300

    Permissions api added

--- draws/api.py
+++ draws/api.py
@@ -1,8 +1,12 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.shortcuts import get_object_or_404
 
 from .models import Draw
 from .serializers import DrawSerializer
+from .permissions import IsOwner
 
 
 class DrawAPIv2Pagination(PageNumberPagination):
@@ -20,3 +24,39 @@ class DrawAPIv2ViewSet(ModelViewSet):
     ).prefetch_related('like')
     serializer_class = DrawSerializer
     pagination_class = DrawAPIv2Pagination
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        obj = get_object_or_404(
+            self.get_queryset(),
+            pk=pk,
+        )
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsOwner(), ]
+
+        return super().get_permissions()
+
+    def partial_update(self, request, *args, **kwargs):
+        draw = self.get_object()
+        serializer = DrawSerializer(
+            instance=draw,
+            data=request.data,
+            many=False,
+            context={'request': request},
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(
+            serializer.data,
+        )
+
+        return super().partial_update(request, *args, **kwargs)
